1f4e896d9d66490e20ccac2002599ab5a9205039
Moriafly <62319450+Moriafly@users.noreply.github.com>
2020-09-14 16:29:04 +0800

    测试

---
+++ app/src/main/java/com/dirror/music/ui/activity/LoginByPhoneActivity.kt
@@ -0,0 +1,12 @@
+package com.dirror.music.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dirror.music.R
+
+class LoginByPhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_by_phone)
+    }
+}
