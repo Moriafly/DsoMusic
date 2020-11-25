9bfe40e9fd06daac50b94ace38efe92dd775bd53
Moriafly <62319450+Moriafly@users.noreply.github.com>
2020-11-25 23:30:36 +0800

    2020年11月25日23:30:34

--- app/src/main/java/com/dirror/music/adapter/DetailPlaylistAdapter.kt
+++ app/src/main/java/com/dirror/music/adapter/DetailPlaylistAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -34,6 +35,11 @@ class DetailPlaylistAdapter(private val songDataList: ArrayList<StandardSongData
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.clSong.animation = AnimationUtils.loadAnimation(holder.itemView.context, R.anim.anim_recycle_item)
+
+
+
         val song = songDataList[position]
         // 1 是要 vip，0 不一定（无语）
 //        if (song.neteaseInfo?.fee == 1) {
