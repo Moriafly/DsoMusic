acc634bc60cddd39cc995db4c84fb6786808ff09
Moriafly <62319450+Moriafly@users.noreply.github.com>
2020-12-12 16:10:44 +0800

    2020年12月12日16:10:42

--- gradlew.bat
+++ gradlew.bat
@@ -13,6 +13,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
