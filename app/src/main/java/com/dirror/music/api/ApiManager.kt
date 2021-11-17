6d4c5cbc59cda251fa3d0a1225086c973d185538
JuanLv <5554102+olbb@users.noreply.github.com>
2021-11-17 18:36:26 +0800

    fix missing api; let playlist and search api use user setting api

--- app/src/main/java/com/dirror/music/api/ApiManager.kt
+++ app/src/main/java/com/dirror/music/api/ApiManager.kt
@@ -45,8 +45,6 @@ const val API_MUSIC_ELEUU = "http://music.eleuu.com"
 
 const val API_AUTU = "https://autumnfish.cn"
 
-const val API_NEW = "http://163.lpddr5.cn"
-
 const val API_LOGIN = API_AUTU
 
 
